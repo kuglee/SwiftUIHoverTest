2c82d44e284410bc8cc66e3f22c8d0b8fb979d8b
Gábor Librecz <kuglee@gmail.com>
2022-06-21 16:06:06 +0200

    Initial Commit

---
+++ HoverTest/HoverTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoverTestApp.swift
+//  HoverTest
+//
+//  Created by Librecz Gábor on 2022. 06. 21..
+//
+
+import SwiftUI
+
+@main
+struct HoverTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
